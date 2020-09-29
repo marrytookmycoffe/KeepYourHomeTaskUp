ae842f0d208c804fef27f85f4e89ddcca960a6c3
I can't write it <mikolaj.frankiewicz@tlen.pl>
2020-09-29 15:41:14 +0200

    pc zrobione

---
+++ Infrastructure/DTO/TaskDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DTO
+{
+    public class TaskDTO
+    {
+        public TimeDTO Time { get; set; }
+        public Guid Room_Id { get; set; }
+        public Guid User_Id { get; set; }
+    }
+}
